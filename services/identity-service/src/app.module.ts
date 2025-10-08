12877d27a556fe012c2437b0fdd4f486e072ec11
Marcus Berggren <berggren.me@outlook.com>
2025-10-08 11:49:26 +0200

    #4 Enforce project wide format constraint with biome

--- services/identity-service/src/app.module.ts
+++ services/identity-service/src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ApiKeysModule } from './api-keys/api-keys.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { RolesModule } from './roles/roles.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { MqttModule } from './mqtt/mqtt.module';
 import { HealthModule } from './health/health.module';
+import { MqttModule } from './mqtt/mqtt.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RolesModule, PrismaModule, MqttModule, HealthModule],
-  controllers: [],
-  providers: [],
+	imports: [
+		AuthModule,
+		UsersModule,
+		RolesModule,
+		PrismaModule,
+		MqttModule,
+		HealthModule,
+		OrganizationsModule,
+		ApiKeysModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
--- services/identity-service/src/roles/roles.module.ts
+++ services/identity-service/src/roles/roles.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { RolesService } from './roles.service';
 
 @Module({
-  providers: [RolesService],
-  controllers: [RolesController]
+	providers: [RolesService],
+	controllers: [RolesController],
 })
 export class RolesModule {}
